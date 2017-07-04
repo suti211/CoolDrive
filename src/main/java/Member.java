9a07ae959592e8d1b9f5a909411f92848474e5ea
redgyuf <faramirex070@gmail.com>
2017-07-04 10:29:33 +0200

    init base project

---
+++ src/main/java/Member.java
@@ -0,0 +1,114 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+//import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("/members")
+//@RequestScoped
+public class Member {
+
+	@GET
+	@Path("/hello")
+	public String sayHello() {
+		return "<h1>Kaka</h1>";
+	}
+
+	@POST
+	@Path("/hello")
+	public Response getHello(@FormParam("message") String msg) {
+		return Response.status(200).entity("asdasdasd: " + msg).build();
+	}
+	
+	@GET
+	@Path("/users?query=shit")
+	public String getUsers(@QueryParam("shit") String msg){
+		return msg;
+	}
+	
+	
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public String listAllMembers() {
+//        return "teszt";
+//    }
+//
+//
+//    @POST
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response createMember(Member member) {
+//
+//        Response.ResponseBuilder builder = null;
+//
+//        try {
+//            // Validates member using bean validation
+//            validateMember(member);
+//
+//            registration.register(member);
+//
+//            // Create an "ok" response
+//            builder = Response.ok();
+//        } catch (ConstraintViolationException ce) {
+//            // Handle bean validation issues
+//            builder = createViolationResponse(ce.getConstraintViolations());
+//        } catch (ValidationException e) {
+//            // Handle the unique constrain violation
+//            Map<String, String> responseObj = new HashMap<>();
+//            responseObj.put("email", "Email taken");
+//            builder = Response.status(Response.Status.CONFLICT).entity(responseObj);
+//        } catch (Exception e) {
+//            // Handle generic exceptions
+//            Map<String, String> responseObj = new HashMap<>();
+//            responseObj.put("error", e.getMessage());
+//            builder = Response.status(Response.Status.BAD_REQUEST).entity(responseObj);
+//        }
+//
+//        return builder.build();
+//    }
+
+
+    /**
+     * Creates a JAX-RS "Bad Request" response including a map of all violation fields, and their message. This can then be used
+     * by clients to show violations.
+     * 
+     * @param violations A set of violations that needs to be reported
+     * @return JAX-RS response containing all violations
+     */
+//    private Response.ResponseBuilder createViolationResponse(Set<ConstraintViolation<?>> violations) {
+//        System.out.println(("Validation completed. violations found: " + violations.size());
+//
+//        Map<String, String> responseObj = new HashMap<>();
+//
+//        for (ConstraintViolation<?> violation : violations) {
+//            responseObj.put(violation.getPropertyPath().toString(), violation.getMessage());
+//        }
+//
+//        return Response.status(Response.Status.BAD_REQUEST).entity(responseObj);
+//    }
+
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<html>
+<body>
+	<h2>Faszfej!</h2>
+	<form action="rest/members/hello" method="post">
+		<input tpye="text" name="message">
+		<input type="submit" name="fck" value="submit">
+	</form>
+	<form action="rest/members/hello/users" method="get">
+	<input type="submit" name="query" value="shit">
+	</form>
+	
+</body>
+</html>
+ 
