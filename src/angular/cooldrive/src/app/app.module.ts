9a15510371bd7068c6b241d7168b4799e1e7ac15
redgyuf <faramirex070@gmail.com>
2017-07-06 14:01:53 +0200

    Router added

--- src/angular/cooldrive/src/app/app.module.ts
+++ src/angular/cooldrive/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
