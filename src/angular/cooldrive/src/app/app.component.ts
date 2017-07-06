74d17db81e68d4cead1e984a92f18139ce6a4f6f
Unknown <suti211@gmail.com>
2017-07-06 14:12:48 +0200

    Implemented route outlet, initialized login and register pages

--- src/angular/cooldrive/src/app/app.component.ts
+++ src/angular/cooldrive/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 //import { Component } from './components/login/login.component';
 import { RouterModule, Routes} from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: ['@import "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";']
 })
 
 //const appRoutes: Routes = [
--- src/angular/cooldrive/src/app/components/login/login.component.ts
+++ src/angular/cooldrive/src/app/components/login/login.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'login',
-  templateUrl: './components/login/login.component.html',
-  styleUrls: ['./components/login/login.component.css']
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 
-export class AppComponent {
+export class LoginComponent {
   title = 'Login';
 }
