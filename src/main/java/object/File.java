b096a81e00f54e4104203dbb825a459fbd4176ef
David Szilagyi <szilagyidav@gmail.com>
2017-07-06 11:37:47 +0200

    File and User classes created by database

---
+++ src/main/java/object/File.java
@@ -0,0 +1,79 @@
+package object;
+
+import java.sql.Date;
+
+/**
+ * Created by David Szilagyi on 2017. 07. 06..
+ */
+public class File {
+    private final int id;
+    private String path;
+    private final double size;
+    private final Date uploadDate;
+    private String fileName;
+    private double maxSize;
+    private final boolean isFolder;
+    private final int ownerId;
+    private final int parentId;
+
+    public File(int id, String path, double size, Date uploadDate, String fileName, double maxSize, boolean isFolder, int ownerId, int parentId) {
+        this.id = id;
+        this.path = path;
+        this.size = size;
+        this.uploadDate = uploadDate;
+        this.fileName = fileName;
+        this.maxSize = maxSize;
+        this.isFolder = isFolder;
+        this.ownerId = ownerId;
+        this.parentId = parentId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public Date getUploadDate() {
+        return uploadDate;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public double getMaxSize() {
+        return maxSize;
+    }
+
+    public boolean isFolder() {
+        return isFolder;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public int getParentId() {
+        return parentId;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setMaxSize(double maxSize) {
+        this.maxSize = maxSize;
+    }
+
+}
---
+++ src/main/java/object/User.java
@@ -0,0 +1,98 @@
+package object;
+
+/**
+ * Created by David Szilagyi on 2017. 07. 06..
+ */
+public class User {
+    private final int id;
+    private final String userName;
+    private String pass;
+    private String email;
+    private boolean validated;
+    private String firstName;
+    private String lastName;
+    private final boolean admin;
+    private int quantity;
+    private int usage;
+
+    public User(int id, String userName, String pass, String email, boolean validated, String firstName, String lastName, boolean admin, int quantity, int usage) {
+        this.id = id;
+        this.userName = userName;
+        this.pass = pass;
+        this.email = email;
+        this.validated = validated;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.admin = admin;
+        this.quantity = quantity;
+        this.usage = usage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean isValidated() {
+        return validated;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getUsage() {
+        return usage;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setValidated(boolean validated) {
+        this.validated = validated;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setUsage(int usage) {
+        this.usage = usage;
+    }
+}
