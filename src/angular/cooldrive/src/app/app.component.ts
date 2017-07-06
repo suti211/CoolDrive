a263af9388849dacb5542ec09f7fa00a1519104d
Unknown <suti211@gmail.com>
2017-07-06 11:34:35 +0200

    Created Login component manually, added components folder

--- src/angular/cooldrive/src/app/app.component.ts
+++ src/angular/cooldrive/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+//import { Component } from './components/login/login.component';
+import { RouterModule, Routes} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+//const appRoutes: Routes = [
+//  {path: 'login', component: 'login'},
+//  {path: '**', component: 'PageNotFoundComponent'}
+//];
+
 export class AppComponent {
   title = 'app';
 }
---
+++ src/angular/cooldrive/src/app/components/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  templateUrl: './components/login/login.component.html',
+  styleUrls: ['./components/login/login.component.css']
+})
+
+export class AppComponent {
+  title = 'Login';
+}
