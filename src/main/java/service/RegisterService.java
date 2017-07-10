14bee105c2f28c5026ecd3e65a7c59236e20e02c
Unknown <suti211@gmail.com>
2017-07-10 12:06:24 +0200

    Testin purpose register component

--- src/main/java/service/RegisterService.java
+++ src/main/java/service/RegisterService.java
@@ -4,23 +4,27 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import dto.Operation;
+import dto.Status;
 import dto.User;
 
 @Path("/register")
 public class RegisterService {
-	
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public User getUser(final User input){
+	@Produces(MediaType.APPLICATION_JSON)
+	public Status getUser(User input) {
 		System.out.println(input.toString());
-		return input;
+		return new Status(Operation.REGISTER, false, "A kurva anyád!");
 	}
-	
+
 	@GET
-	public String test(){
+	public String test() {
 		return "<h1>aha</h1>";
 	}
-	
+
 }
