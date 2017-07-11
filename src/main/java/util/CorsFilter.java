126e2450a07f0c81de0e6aed2951ce9e084c24f7
Unknown <suti211@gmail.com>
2017-07-11 14:31:32 +0200

    Added java 1.8 as source target to pom.xml, added cors filter for testing

---
+++ src/main/java/util/CorsFilter.java
@@ -0,0 +1,18 @@
+package util;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:4200");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+    }
+}
