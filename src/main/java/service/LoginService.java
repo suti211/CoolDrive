61e4b790d5f536c86baa4485438b3f304edaac7f
Unknown <suti211@gmail.com>
2017-07-11 13:18:21 +0200

    Basic Login Service, with session

--- src/main/java/service/LoginService.java
+++ src/main/java/service/LoginService.java
@@ -1,12 +1,18 @@
 package service;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import controller.UserController;
+import dto.Operation;
 import dto.Status;
+import dto.User;
 import util.ConnectionUtil;
 
 @Path("/login")
@@ -16,10 +22,22 @@ public class LoginService {
 
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Status authenticateUser(User input) {
+	public Status authenticateUser(User input, @Context HttpServletRequest request) {
 		
+		int userId = userController.checkUser(input.getUserName(), input.getPass());
 		
-		
+		if(userId != -1){
+			User user = userController.getUser(userId);
+			HttpSession session = request.getSession(true);
+			session.setAttribute("user", user);
+			return new Status(Operation.LOGIN, true, user.getUserName() + " logged in succesfully!");
+		} else {
+			return new Status(Operation.LOGIN, false, "Username or password is SHIT!");
+		}
 	}
-
+	
+	@GET
+	public String test() {
+		return "<h1>aha</h1>";
+	}
 }
