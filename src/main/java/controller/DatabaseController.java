1e50e1ff28e8241e48b6836fa7bd83a6f7130b5f
David Szilagyi <szilagyidav@gmail.com>
2017-07-11 11:16:46 +0200

    created super class for Database connection

---
+++ src/main/java/controller/DatabaseController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import util.ConnectionUtil;
+import java.sql.Connection;
+
+/**
+ * Created by David Szilagyi on 2017. 07. 11..
+ */
+public class DatabaseController {
+    protected Connection con = null;
+
+    public DatabaseController(ConnectionUtil.DatabaseName database) {
+        this.con = ConnectionUtil.getConnection(database);
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+}
