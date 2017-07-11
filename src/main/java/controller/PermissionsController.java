7ee845bc347a5c5f418a7d0d585a3e2e0049cf03
David Szilagyi <szilagyidav@gmail.com>
2017-07-11 11:46:47 +0200

    PermissionsController implemented methods

--- src/main/java/controller/PermissionsController.java
+++ src/main/java/controller/PermissionsController.java
@@ -3,6 +3,9 @@ package controller;
 import dao.PermissionsDao;
 import util.ConnectionUtil;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  * Created by David Szilagyi on 2017. 07. 11..
  */
@@ -13,10 +16,30 @@ public class PermissionsController extends DatabaseController implements Permiss
     }
 
     public boolean addFileToUser(int userId, int fileId) {
+        PreparedStatement ps = null;
+        try {
+            ps = con.prepareStatement("INSERT INTO Permissions(userId, fileId) VALUES (?, ?)");
+            ps.setInt(1, userId);
+            ps.setInt(2, fileId);
+            int success = ps.executeUpdate();
+            return success > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
     public boolean removeFileFromUser(int userId, int fileId) {
+        PreparedStatement ps = null;
+        try {
+            ps = con.prepareStatement("DELETE FROM Permissions WHERE userId = ? AND fileId = ?");
+            ps.setInt(1, userId);
+            ps.setInt(2, fileId);
+            int success = ps.executeUpdate();
+            return success > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 }
