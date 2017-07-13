9a54d694ef7e0017b2e6912610b6c25815a42fff
Unknown <suti211@gmail.com>
2017-07-13 14:15:17 +0200

    Name display

--- src/angular/cooldrive/src/app/components/dashboard/dashboard.component.ts
+++ src/angular/cooldrive/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { slideInOutAnimation } from '../../_animations/slide/slide.animation';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+userName : String = localStorage.key(0);
+
+  constructor() {}
 
   ngOnInit() {
   }
