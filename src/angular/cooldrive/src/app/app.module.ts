8a5e471abd769f4d993ee2d779010b031d2be9ba
Unknown <suti211@gmail.com>
2017-07-13 14:14:19 +0200

    Added the login guard as provider

--- src/angular/cooldrive/src/app/app.module.ts
+++ src/angular/cooldrive/src/app/app.module.ts
@@ -1,1 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';import { NgModule } from '@angular/core';import { RouterModule } from '@angular/router';import { FormsModule } from '@angular/forms';import { HttpModule } from '@angular/http';import { AppComponent } from './app.component';import { LoginComponent } from './components/login/login.component';import { RegisterComponent } from './components/register/register.component';import { DashboardComponent } from './components/dashboard/dashboard.component';import { LoginService } from './service/login.service';import { BrowserAnimationsModule } from '@angular/platform-browser/animations';import {RegisterService} from './service/register.service';@NgModule({  declarations: [    AppComponent,    LoginComponent,    RegisterComponent,    DashboardComponent  ],  imports: [    BrowserAnimationsModule,    BrowserModule,    FormsModule,    HttpModule,    RouterModule.forRoot([      { path: 'login', component: LoginComponent },      { path: 'register', component: RegisterComponent },      { path: 'dashboard', component: DashboardComponent }    ])  ],  providers: [LoginService, RegisterService],  bootstrap: [AppComponent]})export class AppModule { }
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginService } from './service/login.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterService } from './service/register.service';
+import { LoginGuard } from './guard/login.guard';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'dashboard', component: DashboardComponent, canActivate: [LoginGuard] }
+    ])
+  ],
+  providers: [LoginService, RegisterService, LoginGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
