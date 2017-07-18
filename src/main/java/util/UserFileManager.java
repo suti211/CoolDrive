b567e926c1809a9de50ba7fc0e21b067d3d009ed
David Szilagyi <szilagyidav@gmail.com>
2017-07-18 10:18:41 +0200

    UserFileManager started to implement

---
+++ src/main/java/util/UserFileManager.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by David Szilagyi on 2017. 07. 18..
+ */
+public class UserFileManager {
+
+    public static Map<String, Object> saveUserFile(String userName, String fileName) {
+        Map<String, Object> file = new HashMap<>();
+        return file;
+    }
+}
