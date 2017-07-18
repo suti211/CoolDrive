2bf4abe60196e94fde7837cbd03eaa0d0fa176f8
redgyuf <faramirex070@gmail.com>
2017-07-18 14:49:22 +0200

    created storageInfo model

---
+++ src/angular/cooldrive/src/app/model/storage-info.ts
@@ -0,0 +1,11 @@
+export class StorageInfo {
+
+  usage: number;
+  quantity: number;
+
+
+  constructor(usage: number, quantity: number) {
+    this.usage = usage;
+    this.quantity = quantity;
+  }
+}
