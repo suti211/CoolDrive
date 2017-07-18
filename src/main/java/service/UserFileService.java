b0b4d301490d61532b84cefb79b85cd698ca5848
David Szilagyi <szilagyidav@gmail.com>
2017-07-18 18:25:26 +0200

    Token added an id for get files, UserFileService fixed

--- src/main/java/service/UserFileService.java
+++ src/main/java/service/UserFileService.java
@@ -1,19 +1,16 @@
 package service;
 
+import com.google.gson.Gson;
 import controller.UserController;
 import controller.UserFileController;
-import dto.Operation;
-import dto.User;
+import dto.Token;
 import dto.UserFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.ConnectionUtil;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -31,11 +28,12 @@ public class UserFileService {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/getfiles")
-    public List<UserFile> getAllFilesFromFolder(String token, int id, @Context HttpServletRequest request) {
-        LOG.info("getAllFilesFromFolder post method is called with token:{}, id: {}, from: {}", token, id, request.getRemoteAddr());
-        int fileId = id;
-        if (id != -1) {
-            fileId = userController.getUser(token).getId();
+    public List<UserFile> getAllFilesFromFolder(Token token, @Context HttpServletRequest request) {
+        String userToken = token.getToken();
+        int fileId = token.getId();
+        LOG.info("getAllFilesFromFolder post method is called with token:{}, id: {}, from: {}", userToken, fileId, request.getRemoteAddr());
+        if (fileId != -1 || fileId == 0) {
+            fileId = userController.getUser(userToken).getUserHomeId();
         }
         return userFileController.getAllFilesFromFolder(fileId);
     }
