bfcf65f50a10aa6ca219ceb4fc0a4b7d27f8eb50
Unknown <suti211@gmail.com>
2017-07-18 11:04:10 +0200

    Added token validation service

--- src/angular/cooldrive/src/app/service/token.service.ts
+++ src/angular/cooldrive/src/app/service/token.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import { Token } from '../model/token.model';
+import { Status } from '../model/status.model';
 
 @Injectable()
 export class TokenService{
-    loginUrl = "http://localhost:8080/CoolDrive/login";
+    tokenUrl = "http://localhost:8080/CoolDrive/token";
     //loginUrl = "http://demo1158757.mockable.io/loginTest";
 
     constructor(private http:Http){
@@ -20,7 +21,19 @@ export class TokenService{
         let headers = new Headers({'Content-Type' : 'application/json'});
         let options = new RequestOptions({headers : headers});
 
-        return this.http.post(this.loginUrl, bodyString, options)
+        return this.http.post(this.tokenUrl, bodyString, options)
+            .map((res: Response) => res.json())
+            .catch((error: any) => Observable.throw(error.json().error || 'Server Error'));
+    }
+
+    validateToken(token: Token): Observable<Status>{
+        let bodyString = JSON.stringify(token);
+        console.log("body JSON: " + bodyString);
+
+        let headers = new Headers({'Content-Type' : 'application/json'});
+        let options = new RequestOptions({headers : headers});
+
+         return this.http.post(this.tokenUrl, bodyString, options)
             .map((res: Response) => res.json())
             .catch((error: any) => Observable.throw(error.json().error || 'Server Error'));
     }
