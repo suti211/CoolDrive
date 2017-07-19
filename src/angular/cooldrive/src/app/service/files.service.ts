1d6476d35b8434b9174f4410b1bf7a520e225532
redgyuf <faramirex070@gmail.com>
2017-07-19 09:57:08 +0200

    modified file listing to make it work with backend

--- src/angular/cooldrive/src/app/service/files.service.ts
+++ src/angular/cooldrive/src/app/service/files.service.ts
@@ -5,22 +5,56 @@ import { File } from '../model/file.model';
 
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
+import {StorageInfo} from '../model/storage-info';
+import {Token} from '../model/token.model';
 
 @Injectable()
 export class FileService {
   filesUrl = "http://demo3422681.mockable.io/";
+  //filesUrl = "http://192.168.150.29:8080/CoolDrive/files/";
 
   constructor(private http: Http) {
 
+  }
+
+  getStorageInfo(): Observable<StorageInfo> {
+
+    let headers = new Headers({'Content-Type' : 'application/json'});
+    let options = new RequestOptions({headers : headers});
+
+    return this.http.post(this.filesUrl + 'getStorageInfo', '', options)
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server Error'));
   }
 
   getFiles(): Observable<File[]> {
 
     let headers = new Headers({'Content-Type' : 'application/json'});
     let options = new RequestOptions({headers : headers});
-    console.log("asddd");
-    return this.http.post(this.filesUrl, '', options)
+    return this.http.post(this.filesUrl + 'getfiles', '', options)
       .map((res: Response) => res.json())
       .catch((error: any) => Observable.throw(error.json().error || 'Server Error'));
   }
+
+/*
+  getStorageInfo2(token: Token): Observable<StorageInfo> {
+
+  let headers = new Headers({'Content-Type' : 'application/json'});
+  let options = new RequestOptions({headers : headers});
+
+  return this.http.post(this.filesUrl + 'getStorageInfo', token, options)
+  .map((res: Response) => res.json())
+  .catch((error: any) => Observable.throw(error.json().error || 'Server Error'));
+  }
+
+  getFiles2(token: Token): Observable<File[]> {
+
+  let headers = new Headers({'Content-Type' : 'application/json'});
+  let options = new RequestOptions({headers : headers});
+  return this.http.post(this.filesUrl + 'getFiles', token, options)
+  .map((res: Response) => res.json())
+  .catch((error: any) => Observable.throw(error.json().error || 'Server Error'));
+  }
+*/
+
 }
