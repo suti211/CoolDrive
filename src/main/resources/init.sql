1aace2086a02ff20257e4b371746913aebbe563f
David Szilagyi <szilagyidav@gmail.com>
2017-07-27 22:42:43 +0200

    init.sql fixed database creation

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `CoolDrive` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE  IF NOT EXISTS `CoolDrive` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `CoolDrive`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
@@ -42,7 +42,7 @@ CREATE TABLE `Files` (
   KEY `Files_Files_id_fk` (`parentId`),
   CONSTRAINT `Files_Files_id_fk` FOREIGN KEY (`parentId`) REFERENCES `Files` (`id`),
   CONSTRAINT `Files_Users_id_fk` FOREIGN KEY (`ownerId`) REFERENCES `Users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -51,6 +51,7 @@ CREATE TABLE `Files` (
 
 LOCK TABLES `Files` WRITE;
 /*!40000 ALTER TABLE `Files` DISABLE KEYS */;
+INSERT INTO `Files`(`path`, `size`, `uploadDate`, `filename`, `extension`, `maxSize`, `isFolder`, `ownerId`, `parentId`, `label`) VALUES ("D:\\CoolDrive\\Users\\", 0, NOW(), "CoolDrive_Home", "dir", NULL, 1, NULL, NULL, "HOME FOLDER");
 /*!40000 ALTER TABLE `Files` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -68,7 +69,7 @@ CREATE TABLE `Permissions` (
   KEY `Permissions_Users_id_fk` (`userId`),
   CONSTRAINT `Permissions_Files_id_fk` FOREIGN KEY (`fileId`) REFERENCES `Files` (`id`),
   CONSTRAINT `Permissions_Users_id_fk` FOREIGN KEY (`userId`) REFERENCES `Users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -90,20 +91,16 @@ DROP TABLE IF EXISTS `Transactions`;
 CREATE TABLE `Transactions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
-  `firstname` varchar(70) CHARACTER SET latin1 DEFAULT NULL,
-  `lastname` varchar(70) CHARACTER SET latin1 DEFAULT NULL,
   `zip` varchar(45) NOT NULL,
   `city` varchar(45) NOT NULL,
   `address1` varchar(45) NOT NULL,
   `address2` varchar(45) DEFAULT NULL,
   `phone` varchar(45) NOT NULL,
   `bought` varchar(45) NOT NULL,
-  `boughtDate` datetime NOT NULL ,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `Transactions_id_uindex` (`id`),
   KEY `userId_Users.id_idx` (`userId`),
   CONSTRAINT `userId_Users.id` FOREIGN KEY (`userId`) REFERENCES `Users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -124,14 +121,16 @@ DROP TABLE IF EXISTS `Users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(30) CHARACTER SET latin1 NOT NULL,
-  `pass` varchar(70) CHARACTER SET latin1 NOT NULL,
-  `email` varchar(70) CHARACTER SET latin1 NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `pass` varchar(70) NOT NULL,
+  `email` varchar(70) NOT NULL,
   `validated` tinyint(1) NOT NULL,
-  `firstname` varchar(70) CHARACTER SET latin1 DEFAULT NULL,
-  `lastname` varchar(70) CHARACTER SET latin1 DEFAULT NULL,
+  `firstname` varchar(70) DEFAULT NULL,
+  `lastname` varchar(70) DEFAULT NULL,
   `admin` tinyint(1) DEFAULT NULL,
-  `token` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
+  `quantity` double DEFAULT NULL,
+  `usage` double DEFAULT NULL,
+  `token` varchar(45) DEFAULT NULL,
   `registerdate` date DEFAULT NULL,
   `userhomeid` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -139,7 +138,7 @@ CREATE TABLE `Users` (
   UNIQUE KEY `Users_username_uindex` (`username`),
   UNIQUE KEY `Users_email_uindex` (`email`),
   KEY `userhomeid_idx` (`userhomeid`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -160,4 +159,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-24 13:56:05
+-- Dump completed on 2017-07-27 22:28:00
