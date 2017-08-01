8aae3a6fcc8c67fdd6373e3d18d3f4683721b39d
David Szilagyi <szilagyidav@gmail.com>
2017-08-01 15:51:01 +0200

    renamed maxsize to maxSize

--- src/angular/cooldrive/src/app/model/folder.ts
+++ src/angular/cooldrive/src/app/model/folder.ts
@@ -1,13 +1,13 @@
 export class Folder {
   token: string;
   name: string;
-  maxsize: number;
+  maxSize: number;
   label: string;
 
-  constructor(token: string, name: string, maxsize: number, label: string) {
+  constructor(token: string, name: string, maxSize: number, label: string) {
     this.token = token;
     this.name = name;
-    this.maxsize = maxsize;
+    this.maxSize = maxSize;
     this.label = label;
   }
 }
