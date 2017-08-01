f549a2e3f7c9385dea83b2e8ee0ab41b11e857d5
redgyuf <faramirex070@gmail.com>
2017-08-01 13:56:45 +0200

    modified folder model
    added tokenID

--- src/angular/cooldrive/src/app/model/folder.ts
+++ src/angular/cooldrive/src/app/model/folder.ts
@@ -1,9 +1,11 @@
 export class Folder {
+  token: string;
   name: string;
   maxsize: number;
   label: string;
 
-  constructor(name: string, maxsize: number, label: string) {
+  constructor(token: string, name: string, maxsize: number, label: string) {
+    this.token = token;
     this.name = name;
     this.maxsize = maxsize;
     this.label = label;
