76e02e4a12cd0e1e3fcb6b9122d097e727bdf096
David Szilagyi <szilagyidav@gmail.com>
2017-08-01 21:16:36 +0200

    sending email body in html format

--- src/main/java/util/EmailSenderUtil.java
+++ src/main/java/util/EmailSenderUtil.java
@@ -1,11 +1,12 @@
 package util;
 
 import dto.User;
-
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 /**
  * Created by David Szilagyi on 2017. 08. 01..
@@ -28,19 +29,26 @@ public class EmailSenderUtil {
             }
         });
 
+        String regURL = String.format("<a href=http://localhost:8080/CoolDrive/user/verify?email=%s&token=%s>Verify my account</a>", user.getEmail(), token);
+        String text = String.format("Dear %s,<br>Thank you for registration for <b>CoolDrive!</b><br>" +
+                "Your account informations:<br>" +
+                "<hr>Username: %s<br>Starting quantity: 50 MB<br>" +
+                "<hr><br>For start to use our services please verify your account with this link: %s" +
+                "<br><br><br><i>Codecool CoolDrive ©</i>", user.getFirstName() + " " + user.getLastName(), user.getUserName(), regURL);
+
         try {
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+//            mimeBodyPart.setContent(text, "text/html");
+            mimeBodyPart.setText(text, "UTF-8", "html");
+            MimeMultipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(username));
+            message.setContent(multipart);
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(user.getEmail()));
             message.setSubject("Verification e-mail for CoolDrive");
-            String regURL = String.format("http://localhost:8080/CoolDrive/user/verify?email=%s&token=%s", user.getEmail(), token);
-            String text = String.format("Dear %s,\nThank you for registration for CoolDrive!\n" +
-                    "Your account informations:\n" +
-                    "------------------------------\nUsername: %s\nStarting quantity: 50 MB\n" +
-                    "------------------------------\nFor start to use our services please verify your account with this link:\n" +
-                    "%s\n\n\nCodecool CoolDrive ©", user.getFirstName() + " " + user.getLastName(), user.getUserName(), regURL);
-            message.setText(text);
+//            message.setText(text);
             Transport.send(message);
             System.out.println("Done");
         } catch (MessagingException e) {
