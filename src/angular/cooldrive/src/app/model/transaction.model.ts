a6aa8a194809c558ce8f31d9840467c9d4e815bc
Unknown <suti211@gmail.com>
2017-08-02 15:45:42 +0200

    Added billing anddress, id field, and "getters setters" to it

--- src/angular/cooldrive/src/app/model/transaction.model.ts
+++ src/angular/cooldrive/src/app/model/transaction.model.ts
@@ -1,10 +1,12 @@
 export class Transaction{
     userToken: string;
+    id: number;
     firstName: string;
     lastName: string;
     zip: string;
     city: string;
     address1: string;
+    address2: string
     bought: number;
     phone: string;
 
@@ -17,5 +19,21 @@ export class Transaction{
         this.address1 = address;
         this.phone = phone;
         this.bought = storagePack;
+    }
+
+    setBillingAddress(billingAddress: string): void{
+        this.address2 = billingAddress;
+    }
+
+    getBillingAddress(): string{
+        return this.address2;
+    }
+
+    setId(id: number): void{
+        this.id = id;
+    }
+
+    getId(): number{
+        return this.id;
     }
 }
