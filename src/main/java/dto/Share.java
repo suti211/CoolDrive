5054bebe4b01fb8498966926974af3e4db4913bc
David Szilagyi <szilagyidav@gmail.com>
2017-08-07 10:07:01 +0200

    create Share dto

---
+++ src/main/java/dto/Share.java
@@ -0,0 +1,43 @@
+package dto;
+
+/**
+ * Created by David Szilagyi on 2017. 08. 07..
+ */
+public class Share {
+    private String email;
+    private boolean readOnly;
+    private Token token;
+
+    public Share(String email, boolean readOnly, Token token) {
+        this.email = email;
+        this.readOnly = readOnly;
+        this.token = token;
+    }
+
+    public Share() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isReadOnly() {
+        return readOnly;
+    }
+
+    public void setReadOnly(boolean readOnly) {
+        this.readOnly = readOnly;
+    }
+
+    public Token getToken() {
+        return token;
+    }
+
+    public void setToken(Token token) {
+        this.token = token;
+    }
+}
