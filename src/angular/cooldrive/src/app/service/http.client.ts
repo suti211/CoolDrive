8c2bc156aa3b271fba6450764fcf92b6da2a5319
Unknown <suti211@gmail.com>
2017-08-09 17:04:26 +0200

    Implemented a httpClient to append token to our requests

--- src/angular/cooldrive/src/app/service/http.client.ts
+++ src/angular/cooldrive/src/app/service/http.client.ts
@@ -1,5 +1,5 @@
-import {Injectable} from "@angular/core";
-import {Http, Headers} from "@angular/http";
+import {Injectable, NgModule} from "@angular/core";
+import {Http, Headers, RequestOptions} from "@angular/http";
 
 @Injectable()
 export class HttpClient {
@@ -7,17 +7,18 @@ export class HttpClient {
   constructor(private http: Http) {}
 
   createAuthorizationHeader(headers: Headers) {
-    headers.append("token", sessionStorage.getItem("transaction"));
+    headers.append("Authorization" , "Bearer " + sessionStorage.getItem(sessionStorage.key(0)));
   }
 
   get(url: string) {
     let headers = new Headers();
+    let requestOptions = new RequestOptions({headers: headers})
     this.createAuthorizationHeader(headers);
-    return this.http.get(url, {headers: headers });
+    return this.http.get(url, requestOptions);
   }
 
   post(url: string, data: object) {
-    let headers = new Headers();
+    let headers = new Headers({'Content-Type': 'application/json'});
     this.createAuthorizationHeader(headers);
     return this.http.post(url, data, {headers: headers});
   }
