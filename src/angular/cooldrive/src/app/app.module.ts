2e846dd7fce5ae66bc5defd1ef9eabfd4e7eef08
Unknown <suti211@gmail.com>
2017-08-09 17:08:56 +0200

    Added new components, swapped single quotes to double

--- src/angular/cooldrive/src/app/app.module.ts
+++ src/angular/cooldrive/src/app/app.module.ts
@@ -1,28 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { LoginService } from './service/login.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RegisterService } from './service/register.service';
-import { LoginGuard } from './guard/login.guard';
-import { FilesComponent } from './components/files/files.component';
-import { FileService } from './service/files.service';
-import { TokenService } from './service/token.service';
-import { LogoutService } from './service/logout.service';
-import { ExtensionComponent } from './components/storage_extension/extension.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
-import { TransactionService } from './service/transaction.service';
-import { CheckoutService } from './service/checkout.service';
-import { PaymentComponent } from './components/payment/payment.component';
-import { TransactionStatus } from './components/transaction_status/transaction.status.component';
-import { EmailValidation } from './components/emailValidation/emailValidation.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { HttpModule} from "@angular/http";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { LoginService } from "./service/login.service";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RegisterService } from "./service/register.service";
+import { LoginGuard } from "./guard/login.guard";
+import { FilesComponent } from "./components/files/files.component";
+import { FileService } from "./service/files.service";
+import { TokenService } from "./service/token.service";
+import { LogoutService } from "./service/logout.service";
+import { ExtensionComponent } from "./components/storage_extension/extension.component";
+import { CheckoutComponent } from "./components/checkout/checkout.component";
+import { TransactionService } from "./service/transaction.service";
+import { CheckoutService } from "./service/checkout.service";
+import { PaymentComponent } from "./components/payment/payment.component";
+import { TransactionStatus } from "./components/transaction_status/transaction.status.component";
+import { EmailValidation } from "./components/emailValidation/emailValidation.component";
 import {EmailValidationService} from "./service/email-validation.service";
+import {TransactionList} from "./components/maintenance/transactionlist.component";
+import {HttpClient} from "./service/http.client";
+import {UnauthorizedComponent} from "./components/unauthorized/unauthorized.component";
+import { UsersComponent } from './components/users/users.component';
 
 
 @NgModule({
@@ -36,7 +40,10 @@ import {EmailValidationService} from "./service/email-validation.service";
     CheckoutComponent,
     PaymentComponent,
     TransactionStatus,
-    EmailValidation
+    EmailValidation,
+    TransactionList,
+    UnauthorizedComponent,
+    UsersComponent
   ],
   imports: [
     BrowserAnimationsModule,
@@ -44,16 +51,19 @@ import {EmailValidationService} from "./service/email-validation.service";
     FormsModule,
     HttpModule,
     RouterModule.forRoot([
-      { path: 'verify/:token', component: EmailValidation },
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent },
-      { path: 'dashboard', component: DashboardComponent, canActivate: [LoginGuard], children:
+      { path: "verify/:token", component: EmailValidation },
+      { path: "login", component: LoginComponent },
+      { path: "register", component: RegisterComponent },
+      { path: "dashboard", component: DashboardComponent, children:
        [
-          { path: 'files', component: FilesComponent},
-          { path: 'storage', canActivate: [LoginGuard], component: ExtensionComponent},
-          { path: 'checkout', canActivate: [LoginGuard], component: CheckoutComponent},
-          { path: 'payment/:id', canActivate: [LoginGuard], component: PaymentComponent},
-          { path: 'transaction', canActivate: [LoginGuard], component: TransactionStatus},
+          { path: "files", component: FilesComponent},
+          { path: "storage",  component: ExtensionComponent},
+          { path: "checkout",  component: CheckoutComponent},
+          { path: "payment/:id",  component: PaymentComponent},
+          { path: "transaction",  component: TransactionStatus},
+          { path: "users", component: UsersComponent },
+          { path: "transactions", component: TransactionList },
+          { path: "unauthorized", component: UnauthorizedComponent },
         ]
       },
       {path: "**", component: LoginComponent}
@@ -62,7 +72,7 @@ import {EmailValidationService} from "./service/email-validation.service";
     ])
   ],
 
-  providers: [LoginService, RegisterService, LoginGuard, FileService, TokenService, LogoutService, TransactionService, CheckoutService, EmailValidationService],
+  providers: [LoginService, RegisterService, LoginGuard, FileService, TokenService, LogoutService, TransactionService, CheckoutService, EmailValidationService, HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
