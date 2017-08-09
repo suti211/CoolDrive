e9496376bdcd401dfb7d72e6ec4bde5d77dc0567
Unknown <suti211@gmail.com>
2017-08-09 17:07:48 +0200

    Implemented transaction listing component

--- src/angular/cooldrive/src/app/components/maintenance/transactionlist.component.ts
+++ src/angular/cooldrive/src/app/components/maintenance/transactionlist.component.ts
@@ -5,22 +5,23 @@ import {Observable} from "rxjs/Observable";
 
 
 @Component({
-  selector: "app-maintenance",
-  templateUrl: "./maintenance.component.html",
-  styleUrls: ["./maintenance.component.css"],
+  selector: "app-transactionlist",
+  templateUrl: "./transactionlist.component.html",
+  styleUrls: ["./transactionlist.component.css"],
   providers: [AdminService],
 })
 
-export class MaintenanceComponent implements OnInit {
+export class TransactionList implements OnInit {
 
   transactions: Transaction[];
   getTransactions: Observable<Transaction[]>;
+  fasz: boolean;
 
   ngOnInit() {
     this.getTransactions = this.adminService.getTransactions();
     this.getTransactions.subscribe((trans: Transaction[]) => {
       this.transactions = trans;
-      console.log(trans);
+      console.log(this.transactions);
     });
   }
 
