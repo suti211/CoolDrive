30cb4ada29e802a4f67f03dd25fecb7eee229c67
David Szilagyi <szilagyidav@gmail.com>
2017-08-23 13:34:08 +0200

    deleted old angular folder

--- src/angular/cooldrive/src/environments/environment.live.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    urlPrefix: "http://redgyuf.servebeer.com:8080/CoolDrive",
-}
--- src/angular/cooldrive/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  urlPrefix: "http://192.168.150.125:8080/CoolDrive",
-};
