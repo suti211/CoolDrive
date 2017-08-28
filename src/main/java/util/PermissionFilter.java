e509873797e68be405863359af3502d774fcf160
redgyuf <faramirex070@gmail.com>
2017-08-28 10:25:51 +0200

    File download fixed

--- src/main/java/util/PermissionFilter.java
+++ src/main/java/util/PermissionFilter.java
@@ -23,7 +23,7 @@ import dto.User;
 @PreMatching
 @Provider
 public class PermissionFilter extends ControllersFactory implements ContainerRequestFilter{
-    List<String>whiteList = Arrays.asList("/login","/register","/verify","/public");
+    List<String>whiteList = Arrays.asList("/login","/register","/verify","/public","/download");
 
     @Override
     public void filter(ContainerRequestContext containerRequestContext) throws IOException {
